ff90f7143a6a30f53d5ad2a669f8aaf11608e3a2
MyGitHubNumber <1945282561@qq.com>
2019-12-11 17:12:57 +0800

    表单校验完成

---
+++ src/components/examineList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      审批列表
+  </div>
+</template>
+
+<script>
+export default {
+    created() {
+
+        this.axios.post("/admin/getAdminExamineList")
+        .then((res)=>{
+
+            console.log(res)
+        })
+        .catch((res)=>{
+
+        })
+    },
+}
+</script>
+
+<style>
+
+</style>
